6f6dbd3bc8399a31562b13c1d0194cd0f47aa3e7
Nikola <nikola.jevremovic@htecgroup.com>
2018-05-09 16:04:50 +0200

    Tried polymorph behavior of functions, functions in lists, dictionaries, recursion, comprehension, lambda and yield

---
+++ vezba/structures/Functions.py
@@ -0,0 +1,47 @@
+def echo(message):
+    print(message)
+
+
+listOfToDo = [(echo, 'Spam'), (echo, 'Jevra')]
+
+for (func, arg) in listOfToDo:
+    func(arg)
+
+
+func = echo
+func('Hej')
+print(func.__name__)
+
+
+# lambda
+def knights():
+    title = 'Sir'
+    function = lambda x: title + ' ' + x.title()
+    return function
+
+
+act = knights()
+msg = act('robin')
+print(msg)
+
+L = [lambda x: x**2,
+     lambda x: x**3,
+     lambda x: x**4]
+
+print(L[1](3))
+
+
+# yield
+def gen():
+    for i in range(10):
+        X = yield
+        yield X**2
+        print(X)
+
+
+G = gen()
+next(G)
+print(G.send(8))
+next(G)
+print(G.send(3))
+next(G)
---
+++ vezba/structures/mins.py
@@ -0,0 +1,51 @@
+# Trying out polymorph behavior using arbitrary arguments
+def min1(*args):
+    res = args[0]
+    for arg in args[1:]:
+        if arg < res:
+            res = arg
+    return res
+
+
+def min2(*args):
+    tmp = list(args)
+    tmp.sort()
+    return tmp[0]
+
+
+print(min1(3, 4, 1, 2))
+print(min2('bb', 'aa', 'cc'))
+
+
+def minmax(test, *args):
+    res = args[0]
+    for arg in args[1:]:
+        if test(arg, res):
+            res = arg
+    return res
+
+
+def lessthan(x, y): return x < y
+
+
+def grtrthan(x, y): return x > y
+
+
+print(minmax(lessthan, 4, 2, 1, 5, 6, 3))
+print(minmax(grtrthan, 4, 2, 1, 5, 6, 3))
+
+
+# Recursion
+def mysum(L):
+    first, *rest = L
+    return first if not rest else first + mysum(rest)  # ternary expression
+
+
+print(mysum([1, 2, 3, 4, 5]))
+print(mysum(['spam', 'ham', 'eggs']))
+print(mysum(open('Proba').read().split()))
+
+
+# list comprehension
+list = [x for x in range(0, 50) if x % 3 == 0]
+print(list)
